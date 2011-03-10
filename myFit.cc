9a78cfc2e2c142960879b0c192352cfa950cf4aa
tobias schlueter <tobias.schlueter@physik.uni-muenchen.de>
2011-03-10 12:46:15 +0100

    Fit in bins.

--- myFit.cc
+++ myFit.cc
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+#include <stdio.h>
+
 #include "TH2.h"
 #include "TRandom1.h"
 #include "Math/SpecFunc.h"
@@ -11,8 +13,9 @@ using namespace std;
 #include "TFitterMinuit.h"
 #include "TStopwatch.h"
 
-#define NRDEVENTS 1000
-#define NMCEVENTS (4*NRDEVENTS)
+#define NRDEVENTS 100000
+size_t nrdevents;
+#define NMCEVENTS 10000 //(4*NRDEVENTS)
 
 struct wave {
   int l, m;
@@ -41,14 +44,19 @@ typedef vector<coherent_waves> waveset;
 
 class event {
 public:
+  double mass;
+  double tPrime;
   double theta;
   double phi;
 
-  event() { theta = phi = 0; }
+  event() { mass = tPrime = theta = phi = 0; }
   /*
   event(const event& other) { theta = other.theta; phi = other.phi; }
   */
-  event(double th, double ph) { theta = th; phi = ph; }
+  event(double th, double ph) { mass = tPrime = 0; theta = th; phi = ph; }
+  event(double mass_, double tPrime_, double th, double ph)
+  { mass = mass_; tPrime = tPrime_; theta = th; phi = ph; }
+  event(const event& o) { mass = o.mass; tPrime = o.tPrime; theta = o.theta; phi = o.phi; }
 
   double decayAmplitude(int reflectivity, const wave& w) const
   { return decayAmplitude(reflectivity, w.l, w.m); };
@@ -71,7 +79,7 @@ public:
   //double operator() (const std::vector<double>& x) const;
   double Up() const { return 0.5; }
 
-private:
+public:
   double
   decay(int reflectivity, int l, int m, double theta, double phi) const;
 
@@ -110,9 +118,9 @@ event::decayAmplitude(int reflectivity, int l, int m) const
   double spherical = ROOT::Math::sph_legendre(l, m, this->theta);
 
   // This absorbs the factor 2 from e^i \pm e^-i = 2 {i sin, cos}
-  double factor = 1;
+  double factor = 1; //sqrt(nrdevents);
   if (m != 0)
-    factor = sqrt(2.);    
+    factor *= sqrt(2.);    
 
   if (reflectivity == +1)
     return factor*spherical*sin(m*this->phi);
@@ -180,8 +188,14 @@ likelihood::MCweight(int reflectivity, const wave& w1, const wave& w2) const
   // Uses Kahan's summation
   double sum = 0;
   double c = 0;
+  int countRejected = 0;
   for (size_t i = 0; i < NMCEVENTS /*MCevents.size()*/; i++)
     {
+      if (0 && cos(MCevents[i].theta) < -.8)
+	{
+	  countRejected++;
+	  continue;
+	}
       // Forming this sum as REAL sum and with no conjugate, because
       // the form of the decay amplitudes allows this.  This is not
       // the most general form!
@@ -191,8 +205,11 @@ likelihood::MCweight(int reflectivity, const wave& w1, const wave& w2) const
       sum = t;
     }
 
-  return (weights[id] = sum / NMCEVENTS);
+  return (weights[id] = sum / (NMCEVENTS - countRejected));
 }
+
+double massLow = 0;
+double massHigh = 9999;
 
 double
 likelihood::calc_likelihood(const vector<double>& x) const
@@ -222,11 +239,13 @@ likelihood::calc_likelihood(const vector<double>& x) const
       waveset_base = idx1;
     }
 
-  // Uses Kahan summation
+  // Uses Kahan summation2
   double sumRD = 0;
   double c = 0;
-  for (size_t i = 0; i < NRDEVENTS /*RDevents.size()*/; i++)
+  for (size_t i = 0; i < nrdevents; i++)
     {
+      if (RDevents[i].mass < massLow || RDevents[i].mass > massHigh)
+	continue;
       double y = log(probabilityDensity(x, RDevents[i])) - c;
       double t = sumRD + y;
       c = (t - sumRD) - y;
@@ -242,7 +261,6 @@ likelihood::calc_likelihood(const vector<double>& x) const
 double
 likelihood::operator()(const vector<double>& x) const
 {
-
   return -this->calc_likelihood(x);
 }
 
@@ -259,12 +277,10 @@ myFit()
 
   vector<wave> negative;
   negative.push_back(wave(0,0));
-  /*
   negative.push_back(wave(1,0));
   negative.push_back(wave(1,1));
   negative.push_back(wave(2,0));
   negative.push_back(wave(2,1));
-  */
 
   coherent_waves wsPos, wsNeg;
   wsPos.reflectivity = +1;
@@ -279,16 +295,29 @@ myFit()
   ws.push_back(wsPos);
   ws.push_back(wsNeg);
 
-  TH2* hRD = new TH2I("hRD", "RD", 10, -1, 1, 10, -M_PI, M_PI);
-  TH2* hMC = new TH2I("hMC", "MC", 10, -1, 1, 10, -M_PI, M_PI);
+  TH2* hRD = new TH2D("hRD", "RD", 10, -1, 1, 10, -M_PI, M_PI);
+  TH2* hMC = new TH2D("hMC", "MC", 10, -1, 1, 10, -M_PI, M_PI);
+  TH1* hMass = new TH1D("hMass", "mass distribution",
+			250, 0.5, 3);
 
   //vector<event> RDevents(2500, event(0,0));
-  for (int i = 0; i < NRDEVENTS; i++)
+
+  FILE* fd = fopen("dataEtaPpi.txt", "r");
+  char line[99999];
+  nrdevents = 0;
+  while (fgets(line, 99999, fd))
     {
-      event e(acos(gRandom->Uniform(-1,1)), gRandom->Uniform(-4*atan(1),4*atan(1)));
-      RDevents[i] = e;
-      hRD->Fill(cos(e.theta), e.phi);
+      double m, tPr, theta, phi;
+      sscanf(line, "%lf %lf %lf %lf", &m, &tPr, &theta, &phi);
+
+      hMass->Fill(m);
+      event e(m, tPr, theta, phi);
+      RDevents[nrdevents++] = e;
+      hRD->Fill(cos(theta), phi);
     }
+  fclose(fd);
+  cout << "read " << nrdevents << " RD events" << endl;
+  //nrdevents = nrdevents / 10;
 
   //vector<event> MCevents(10000, event(0,0));
   for (int i = 0; i < NMCEVENTS; i++)
@@ -304,42 +333,104 @@ myFit()
   TFitterMinuit* minuit = new TFitterMinuit();
   minuit->SetMinuitFCN(&myL);
 
-  for (int i = 0; i < 5; i++)
+  struct {
+    const char* name;
+    double value;
+    bool fixed;
+  }
+  startingValues[14] = { { "Rea(+,2,1)", gRandom->Uniform(5), false },
+			 { "Ima(+,2,1)", 0, true },
+			 { "Rea(+,1,1)", gRandom->Uniform(5), false },
+			 { "Ima(+,1,1)", gRandom->Uniform(5), false },
+			 { "Rea(-,0,0)", gRandom->Uniform(5), false },
+			 { "Ima(-,0,0)", 0, true },
+			 { "Rea(-,1,0)", gRandom->Uniform(5), false },
+			 { "Ima(-,1,0)", gRandom->Uniform(5), false },
+			 { "Rea(-,1,1)", gRandom->Uniform(5), false },
+			 { "Ima(-,1,1)", gRandom->Uniform(5), false },
+			 { "Rea(-,2,0)", gRandom->Uniform(5), false },
+			 { "Ima(-,2,0)", gRandom->Uniform(5), false },
+			 { "Rea(-,2,1)", gRandom->Uniform(5), false },
+			 { "Ima(-,2,1)", gRandom->Uniform(5), false } };
+
+  int nBins = 40;
+  double binWidth = 0.05;
+
+  TH1* hDwave = new TH1D("hDwave", "D wave intensity",
+			 nBins, 0.65, 0.65 + nBins*binWidth);
+  TH1* hPwave = new TH1D("hPwave", "P wave intensity",
+			 nBins, 0.65, 0.65 + nBins*binWidth);
+  TH1* hPhase = new TH1D("hPhase", "D - P phase",
+			 nBins, 0.65, 0.65 + nBins*binWidth);
+  TH1* hIntensity = new TH1D("hIntensity", "total intensity as predicted",
+			     nBins, 0.65, 0.65 + nBins*binWidth);
+
+  TStopwatch fulltime;
+  fulltime.Start();
+  for (int i = 0; i < nBins; i++)
     {
+      massLow = 0.65 + i*binWidth;
+      massHigh = 0.65 + (i+1)*binWidth;
       sw.Start();
 
       minuit->Clear();
 
-      minuit->SetParameter(0,"Rea(+,2,1)", 0, 0, 0, 0);
-      minuit->SetParameter(1,"Ima(+,2,1)", 0, 0, 0, 0);
-      minuit->SetParameter(2,"Rea(+,1,1)", 0, 0, 0, 0);
-      minuit->SetParameter(3,"Ima(+,1,1)", 0, 0, 0, 0);
-      /*
-      minuit->SetParameter(0,"Rea(+,2,1)", gRandom->Uniform(-10,10), 0.1, 0, 0);
-      minuit->SetParameter(1,"Ima(+,2,1)", 0, 0.1, 0, 0);
-      minuit->FixParameter(1);
-      minuit->SetParameter(2,"Rea(+,1,1)", gRandom->Uniform(-10,10), 0.1, 0, 0);
-      minuit->SetParameter(3,"Ima(+,1,1)", gRandom->Uniform(-10,10), 0.1, 0, 0);
-      */
-      minuit->SetParameter(4,"Rea(-,0,0)", gRandom->Uniform(-10,10), 0.5, 0, 0);
-      minuit->SetParameter(5,"Ima(-,0,0)",   0, 0.1, 0, 0);
-      minuit->FixParameter(5);
-      /*
-      minuit->SetParameter(6,"Rea(-,1,0)", gRandom->Uniform(-10,10), 0.1, 0, 0);
-      minuit->SetParameter(7,"Ima(-,1,0)", gRandom->Uniform(-10,10), 0.1, 0, 0);
-      minuit->SetParameter(6,"Rea(-,1,1)", gRandom->Uniform(-10,10), 0.1, 0, 0);
-      minuit->SetParameter(7,"Ima(-,1,1)", gRandom->Uniform(-10,10), 0.1, 0, 0);
-      minuit->SetParameter(6,"Rea(-,2,0)", gRandom->Uniform(-10,10), 0.1, 0, 0);
-      minuit->SetParameter(7,"Ima(-,2,0)", gRandom->Uniform(-10,10), 0.1, 0, 0);
-      minuit->SetParameter(6,"Rea(-,2,1)", gRandom->Uniform(-10,10), 0.1, 0, 0);
-      minuit->SetParameter(7,"Ima(-,2,1)", gRandom->Uniform(-10,10), 0.1, 0, 0);
-      */
+      for (int j= 0; j < 14; j++)
+	{
+	  minuit->SetParameter(j, startingValues[j].name,
+			       startingValues[j].value, 1, 0, 0);
+	  if (startingValues[j].fixed)
+	    minuit->FixParameter(j);
+	}
 
       minuit->CreateMinimizer();
       int iret = minuit->Minimize();
       sw.Stop();
       cout << "iret = " << iret << " after " << sw.CpuTime() << " s." << endl;
+      if (iret == 0)
+	{
+	  for (int j = 0; j < minuit->GetNumberTotalParameters(); j++)
+	    startingValues[j].value = minuit->GetParameter(j);
+
+	  complex<double> aDwave(startingValues[0].value,
+				 startingValues[1].value);
+	  complex<double> aPwave(startingValues[2].value,
+				 startingValues[3].value);
+
+	  hDwave->SetBinContent(i+1,norm(aDwave));
+	  hDwave->SetBinError(i+1, 2*abs(aDwave)*minuit->GetParError(0));
+	  hPwave->SetBinContent(i+1,norm(aPwave));
+	  double error = 2*(sqrt(pow(real(aPwave) * minuit->GetParError(2), 2)
+			       + pow(imag(aPwave) * minuit->GetParError(3), 2)
+			       + (2*real(aPwave)*imag(aPwave)
+				  * minuit->GetCovarianceMatrixElement(2, 3))));
+	  hPwave->SetBinError(i+1, error);
+
+	  //			      abs(aPwave)*(minuit->GetParError(2)/ real(aPwave) + minuit->GetParError(3) / imag(aPwave)));
+	  hPhase->SetBinContent(i+1,arg(aDwave / aPwave));
+
+	  vector<double> result;
+	  for (int iPar = 0; iPar < minuit->GetNumberTotalParameters(); iPar++)
+	    {
+	      result.push_back(minuit->GetParameter(iPar));
+	    }
+
+	  double intensity = 0;
+	  for (int ix = 0; ix < 100; ix++)
+	    {
+	      double x = -1 + 2./100*ix;
+	      for (int iy = 0; iy < 100; iy++)
+		{
+		  double y = -M_PI + 2*M_PI/100*iy;
+		  intensity += myL.probabilityDensity(result, acos(x), y);
+		}
+	    }
+	  hIntensity->SetBinContent(i + 1, intensity / 10000);
+	}
     }
+
+  fulltime.Stop();
+  cout << "took " << fulltime.CpuTime() << " s CPU time, " << fulltime.RealTime() << " s wall time" << endl;
 
   vector<double> result;
   for (int i = 0; i < minuit->GetNumberTotalParameters(); i++)
@@ -347,7 +438,7 @@ myFit()
       result.push_back(minuit->GetParameter(i));
     }
 
-  TH2* hPredict = new TH2I("hPredict", "prediction", 100, -1, 1, 100, -M_PI, M_PI);
+  TH2* hPredict = new TH2D("hPredict", "prediction", 100, -1, 1, 100, -M_PI, M_PI);
   for (int ix = 0; ix < 100; ix++)
     {
       double x = -1 + 2./100*ix;
@@ -357,196 +448,6 @@ myFit()
 	  hPredict->SetBinContent(ix + 1, iy + 1, myL.probabilityDensity(result, acos(x), y));
 	}
     }
-  hPredict->Draw();
+  hPredict->Draw("surf3");
       
 }
-
-#if 0
-max = 4;
-(*            s eta    l  m *)
-indices = { { 1,  1, {{1, 1},
-                      {2, 1} (*,
-                      {2, 2},
-                      {4, 1} *) } },
-            (* {-1,  1, {{2, 2}}}, *)
-            { 1, -1, {{0, 0},
-                      {1, 0},
-                      {2, 0},
-                      {1, 1},
-                      {2, 1}}}};
-
-thetam[0] = 1/2;
-thetam[m_] := 1/Sqrt[2]
-
-a[s_,eta_,l_,m_] := rea[s,eta,l,m] + I ima[s,eta,l,m]
-
-decay[eta_, l_,m_,theta_,phi_] := thetam[m] *
-       (WignerD[{l,0,m},theta,phi] - eta (-1)^m WignerD[{l, 0, -m}, theta,phi])
-
-coherentSum[indices_, theta_, phi_] := \
-     (* indices = {s, eta, {{l,m}, {l,m}, {l, m}, ...}} *)
-     Sum[ a[indices[[1]], indices[[2]], wave[[1]], wave[[2]]] \
-         * decay[indices[[2]], wave[[1]], wave[[2]], theta, phi],
-         {wave, indices[[3]]}];
-
-probabilityDensity[theta_,phi_] := \
-     Sum[Abs[coherentSum[idx, theta, phi]]^2, {idx, indices}]
-
-NMCEvent = 100000;
-mctheta = ArcCos[RandomVariate[UniformDistribution[{-1, 1}], NMCEvent]];
-mcphi = RandomVariate[UniformDistribution[{0, 2Pi}], NMCEvent];
-
-(* <<mcweights_not_backwards *)
-Get["mcweights"]
-
-mcweight[eta_,l1_,m1_,l2_,m2_] := \
-   mcweight[eta,l1,m1,l2,m2] = mcw[eta,l1,m1,l2,m2] (* = \
-   1/NMCEvent*Sum[decay[eta,l1,m1,mctheta[[i]],mcphi[[i]]]*
-                  Conjugate[decay[eta,l2,m2,mctheta[[i]],mcphi[[i]]]],
-                  {i,NMCEvent}] *)
-
-     //(* data = ReadList["data.txt", Number]; *) (* "Hauke_PmEta.txt", Number]; *) (*dataHauke.txt", Number];*)
-
-data = ReadList["Hauke_PmEta.txt", Number];
-{mass, tprime, thetaAll, phiAll} = Transpose[Partition[data, 4]];
-
-NEvent = Length[theta];
-
-(*
-theta = ArcCos[RandomVariate[UniformDistribution[{-1, 1}], NEvent]];
-phi = RandomVariate[UniformDistribution[{0, 2Pi}], NEvent];
-*)
-
-(* probabilityDensity[theta[[1]], phi[[1]]] *)
-
-
-
-Print["binning " <> ToString[Length[mass]] <> " events"]
-
-mBinned = {};
-tpBinned = {};
-thetaBinned = {};
-phiBinned = {};
-
-For[i=0, i<40, i++,
-    massbinlow = 0.7 (* 1.1 *) + i*0.05;
-    massbinhigh = 0.7 (* 1.1 *) + (i+1)*0.05;
-
-    mBin = {};
-    tpBin = {};
-    thetaBin = {};
-    phiBin = {};
-    NEvent = 0;
-    For[j = 0, j<Length[mass], j++,
-        If[mass[[j]] <= massbinhigh && mass[[j]] > massbinlow
-           (* && thetaAll[[j]] > -0.8 *)
-           && tprime[[j]] > 0.6 (* && tprime[[j]] < 0.6 *),
-           AppendTo[mBin, mass[[j]]];
-           AppendTo[tpBin, tprime[[j]]];
-           AppendTo[thetaBin, thetaAll[[j]]];
-           AppendTo[phiBin, phiAll[[j]]]
-          ]
-       ];
-    AppendTo[mBinned, mBin];
-    AppendTo[tpBinned, tpBin];
-    AppendTo[thetaBinned, thetaBin];
-    AppendTo[phiBinned, phiBin];
-]
-
-Print["data binned in " <> ToString[Length[thetaBinned]] <> " bins"];
-
-val = {};
-sol = {};
-
-For[i=1, i < Length[thetaBinned], i++,
-    NEvent = Length[thetaBinned[[i]]];
-    Print[Length[thetaBinned[[i]]]];
-    mcpart = Re@Sum[a[block[[1]], block[[2]], wave1[[1]], wave1[[2]]]
-                    * Conjugate[
-                       a[block[[1]], block[[2]], wave2[[1]], wave2[[2]]] *
-                       mcweight[block[[2]], wave1[[1]], wave1[[2]],
-                                            wave2[[1]], wave2[[2]]]],
-                 {block, indices}, {wave1, block[[3]]}, {wave2, block[[3]]}];
-
-    likelihood = (Sum[Re @ Log[probabilityDensity[thetaBinned[[i]][[j]],
-                                                  phiBinned[[i]][[j]]]],
-                      {j, 1, Length[thetaBinned[[i]]]}]
-                  - mcpart);
-
-    result = NMaximize[{likelihood, ima[1,1,2,1] == 0, ima[1,-1,1,0] == 0},
-
-                                 { rea[1,1,1,1], ima[1,1,1,1],
-                                   rea[1,1,2,1], ima[1,1,2,1],
-                                   (* rea[1,1,4,1], ima[1,1,4,1], *)
-                                   rea[1,-1,0,0], ima[1,-1,0,0],
-                                   rea[1,-1,1,0], ima[1,-1,1,0],
-                                   rea[1,-1,2,0], ima[1,-1,2,0],
-                                   rea[1,-1,1,1], ima[1,-1,1,1],
-                                   rea[1,-1,2,1], ima[1,-1,2,1] }];
-(*
-Table[{rea[s,eta,l,m], ima[s,eta,l,m]},
-                     {s, 1, 1, 2}, {eta, -1, 1, 2},
-                     {l, 0, lmax}, {m, (eta + 1)/2, l}]//Flatten];
-*)
-    AppendTo[val, {NEvent, N[(# Log[#] - #)&[NEvent]], result[[1]]}];
-    AppendTo[sol, result[[2]]];
-
-]
-
-Print[val]
-Print[sol]
-
-Gwave = ListPlot[Abs[rea[1,1,4,1] + I ima[1,1,4,1]] /. sol,
-                DisplayFunction->Identity, ImageSize->{400,300}];
-
-Dwave = ListPlot[Abs[rea[1,1,2,1] + I ima[1,1,2,1]] /. sol,
-                DisplayFunction->Identity, ImageSize->{400,300}];
-
-Dwave2 = ListPlot[Abs[rea[1,1,2,2] + I ima[1,1,2,2]] /. sol,
-                DisplayFunction->Identity, ImageSize->{400,300}];
-
-Pwave = ListPlot[Abs[rea[1,1,1,1] + I ima[1,1,1,1]] /. sol,
-                DisplayFunction->Identity, ImageSize->{400,300}];
-
-phase = ListPlot[Arg[(rea[1,1,2,1] + I ima[1,1,2,1]) / (rea[1,1,1,1] + I ima[1,1,1,1])] /. sol,
-                DisplayFunction->Identity, ImageSize->{400,300}];
-
-Export["plots.png", GraphicsGrid[{{Pwave, Dwave}, {Dwave2, phase}}]]
-
-predict1 = Plot3D[probabilityDensity[ArcCos[cth], phi] /. {rea[_,+1,_,_] -> 0, ima[_,+1,_,_] -> 0} /. sol[[13]],
-                 {cth, -1, 1}, {phi, -Pi, Pi},
-                 DisplayFunction->Identity, ImageSize->{400,300}];
-
-predict2 = Plot3D[probabilityDensity[ArcCos[cth], phi] /. {rea[_,+1,_,_] -> 0, ima[_,+1,_,_] -> 0} /. sol[[14]],
-                 {cth, -1, 1}, {phi, -Pi, Pi},
-                 DisplayFunction->Identity, ImageSize->{400,300}];
-
-predict3 = Plot3D[probabilityDensity[ArcCos[cth], phi] /. {rea[_,-1,_,_] -> 0, ima[_,-1,_,_] -> 0} /. sol[[13]],
-                  {cth, -1, 1}, {phi, -Pi, Pi},
-                  DisplayFunction->Identity, ImageSize->{400,300}];
-
-predict4 = Plot3D[probabilityDensity[ArcCos[cth], phi] /. {rea[_,-1,_,_] -> 0, ima[_,-1,_,_] -> 0} /. sol[[14]],
-                  {cth, -1, 1}, {phi, -Pi, Pi},
-                  DisplayFunction->Identity, ImageSize->{400,300}];
-
-Export["predictions.png", GraphicsGrid[{{predict1, predict2}, {predict3, predict4}}]];
-
-data1 = Histogram3D[{Cos[thetaBinned[[12]]], phiBinned[[12]]} //Transpose,
-                  DisplayFunction->Identity, ImageSize->{400,300}];
-data2 = Histogram3D[{Cos[thetaBinned[[13]]], phiBinned[[13]]} //Transpose,
-                  DisplayFunction->Identity, ImageSize->{400,300}];
-data3 = Histogram3D[{Cos[thetaBinned[[14]]], phiBinned[[14]]} //Transpose,
-                  DisplayFunction->Identity, ImageSize->{400,300}];
-data4 = Histogram3D[{Cos[thetaBinned[[15]]], phiBinned[[15]]} //Transpose,
-                  DisplayFunction->Identity, ImageSize->{400,300}];
-
-Export["data.png", GraphicsGrid[{{data1, data2}, {data3, data4}}]];
-
-
-(* Save["mcweights_not_backwards", mcw] *)
-(* Save["mcweights", mcw]*)
-
-Save["solution.m", sol]
-
-Quit[]
-#endif
