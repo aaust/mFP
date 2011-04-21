c4c9f2a2a9d58b287432281fd724f74640e602bc
tobias schlueter <tobias.schlueter@physik.uni-muenchen.de>
2011-04-21 16:46:51 +0200

    Utility function for decomposing a moment, still the normalization is wrong ...

--- 3j.cc
+++ 3j.cc
@@ -12,6 +12,8 @@
 
 using namespace std;
 
+#include "wave.h"
+
 // Prime number list taken from http://www.mathematical.com/primes0to1000k.html
 static const int primes[] = { 2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,
 			      103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,
@@ -534,6 +536,47 @@ decompose(int lmax, int mmax)
 		}
 	    }
 	  cout << endl;
+	}
+    }
+}
+
+
+// Get decomposition of H(LM) in terms of the waveset ws.
+void
+decomposeMoment(int L, int M, const waveset& ws)
+{
+  for (size_t iWs = 0; iWs < ws.size(); iWs++)
+    {
+      int eps = ws[iWs].reflectivity;
+
+      const vector<wave>& w = ws[iWs].waves;
+      for (size_t iW1 = 0; iW1 < w.size(); iW1++)
+	{
+	  const wave& w1 = w[iW1];
+	  for (size_t iW2 = 0; iW2 < w.size(); iW2++)
+	    {
+	      const wave& w2 = w[iW2];
+	      fraction threeJ1 = threeJalgebraically(L, w1.l, w2.l, 0, 0, 0);
+	      if (threeJ1.isZero())
+		continue;
+
+	      int sign = pow(-1,w1.m+M);
+	      int sign1 = pow(-1,w1.m+1);
+	      int sign2 = pow(-1,w2.m+1);
+	      int sign12 = pow(-1,w1.m+w2.m);
+	      fraction parentheses = (threeJalgebraically(L, w1.l, w2.l, -M, -w1.m, w2.m)
+				      + threeJalgebraically(L, w1.l, w2.l, -M, w1.m, w2.m) * eps * sign1
+				      + threeJalgebraically(L, w1.l, w2.l, -M, -w1.m, -w2.m) * eps * sign2
+				      + threeJalgebraically(L, w1.l, w2.l, -M, w1.m, -w2.m) * sign12) * sign;
+	      if (parentheses.isZero())
+		continue;
+
+	      cout << " + "
+		   << (theta_square(w1.m)*theta_square(w2.m)*fraction((2*w1.l+1)*(2*w2.l+1), 1)
+		       *threeJ1*parentheses)
+		   << "*rho(eps = " << eps << ", " << w1.l << ", " << w1.m << ", " << w2.l << ", " << w2.m
+		   << ")";
+	    }
 	}
     }
 }
