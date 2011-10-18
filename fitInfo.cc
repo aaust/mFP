f483be38802db84a2237cab59212e702d5e562d3
tobias schlueter <tobias.schlueter@physik.uni-muenchen.de>
2011-10-18 17:37:59 +0200

    Add some more stuff to fitInfo structure.

--- fitInfo.cc
+++ fitInfo.cc
@@ -2,8 +2,9 @@
 
 #include "fitInfo.h"
 
-fitInfo::fitInfo(const std::vector<tStartingValue>& fitVars, size_t nBins_, size_t nVars_)
-  : nBins(nBins_), nVars(nVars_)
+fitInfo::fitInfo(const std::vector<tStartingValue>& fitVars, size_t nBins_, double threshold_,
+		 double binWidth_, size_t nVars_)
+  : nBins(nBins_), threshold(threshold_), binWidth(binWidth_), nVars(nVars_)
 {
   for (size_t i = 0; i < fitVars.size(); i++)
     {
--- fitInfo.h
+++ fitInfo.h
@@ -20,17 +20,24 @@ class fitInfo : public TObject
 {
 public:
   fitInfo() {};
-  fitInfo(const std::vector<tStartingValue>& fitVars, size_t nBins_, size_t nVars_);
+  fitInfo(const std::vector<tStartingValue>& fitVars, size_t nBins_, double threshold_,
+	  double binWidth_, size_t nVars_);
 
   size_t getNvars() const { return nVars; }
 
   const std::vector<std::string>& getParamNames() const { return paramNames; }
   const std::vector<bool>& getFixed() const { return fixed; }
   size_t getNbins() const { return nBins; }
+  double getThreshold() const { return threshold; }
+  double getBinWidth() const { return binWidth; }
+  double getLower() const { return threshold; }
+  double getUpper() const { return threshold + nBins*binWidth; }
 private:
   std::vector<std::string> paramNames;
   std::vector<bool> fixed;
-  size_t nBins, nVars;
+  size_t nBins;
+  double threshold, binWidth;
+  size_t nVars;
 
   ClassDef(fitInfo, 1)
 };
