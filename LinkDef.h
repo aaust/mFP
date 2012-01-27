903a9adcf5e16bc8bc45f50fc8289a7e21b56b09
tobias schlueter <tobias.schlueter@physik.uni-muenchen.de>
2012-01-27 11:37:13 +0100

    Add missing file, a few more histograms for my purpose.

---
+++ LinkDef.h
@@ -0,0 +1,8 @@
+#ifdef __CINT__
+
+#pragma link C++ class wave+;
+#pragma link C++ class coherent_waves+;
+#pragma link C++ class waveset+;
+
+#endif
+
--- predict.cc
+++ predict.cc
@@ -68,7 +68,6 @@ main()
       cerr << "Tree without user info" << endl;
       return 1;
     }
-  cout << "info with " << info->getWaveSet().getNwaves() << " waves" << endl;
 
   double *values = new double[info->getNvars()];
   double massLow, massHigh;
@@ -129,13 +128,22 @@ main()
   t->SetBranchAddress("costhGJ", &costh);
   t->SetBranchAddress("phiGJ", &phi);
 
-  float pvx;
-  float pvy;
-  float pvz;
-  t->SetBranchAddress("pvx", &pvx);
-  t->SetBranchAddress("pvy", &pvy);
-  t->SetBranchAddress("pvz", &pvz);
-
+  float pvx; t->SetBranchAddress("pvx", &pvx);
+  float pvy; t->SetBranchAddress("pvy", &pvy);
+  float pvz; t->SetBranchAddress("pvz", &pvz);
+  float pg1X; t->SetBranchAddress("pg1X", &pg1X);
+  float pg1Y; t->SetBranchAddress("pg1Y", &pg1Y);
+  float pg1Z; t->SetBranchAddress("pg1Z", &pg1Z);
+  float pg1E; t->SetBranchAddress("pg1E", &pg1E);
+  float pg2X; t->SetBranchAddress("pg2X", &pg2X);
+  float pg2Y; t->SetBranchAddress("pg2Y", &pg2Y);
+  float pg2Z; t->SetBranchAddress("pg2Z", &pg2Z);
+  float pg2E; t->SetBranchAddress("pg2E", &pg2E);
+  float pPimX; t->SetBranchAddress("pPimX", &pPimX);
+  float pPimY; t->SetBranchAddress("pPimY", &pPimY);
+  float pPimZ; t->SetBranchAddress("pPimZ", &pPimZ);
+  float pPimE; t->SetBranchAddress("pPimE", &pPimE);
+  
   TFile *fOutput = TFile::Open("predict.root", "RECREATE");
 
   for (Long_t i = 0; i < t->GetEntries(); i++)
@@ -177,9 +185,24 @@ main()
       gHist.Fill("hMvsCosth", "m vs cos th",
 		 info->getNbins(), info->getLower(), info->getUpper(), 100, -1, 1,
 		 mX, costh, weight);
-      gHist.Fill("hpvz", "pvz",
-		 500, -100, 0,
+      gHist.Fill("hpvz", "pvz", 500, -100, 0,
 		 pvz, weight);
+      gHist.Fill("htPr", "t'", 1000, 0, 1,
+		 tPr, weight);
+      gHist.Fill("hXYECAL", "XY ECAL", 200, -.04, .04, 200, -0.04, 0.04,
+		 pg1X / pg1Z, pg1Y / pg1Z, weight);
+      gHist.Fill("hXYECAL", "XY ECAL", 200, -.04, .04, 200, -0.04, 0.04,
+		 pg2X / pg2Z, pg2Y / pg2Z, weight);
+      gHist.Fill("hEvsAngle", "E vs angle", 200, 0, 200, 100, 0, 0.06,
+		 pg1E, hypot(pg1X, pg1Y) / pg1Z, weight);
+      gHist.Fill("hEvsAngle", "E vs angle", 200, 0, 200, 100, 0, 0.06,
+		 pg2E, hypot(pg2X, pg2Y) / pg2Z, weight);
+      if (mX < 2)
+	{
+	  gHist.Fill("hpPimE", "energy of non-eta' #pi-",
+		     500, 0, 200,
+		     pPimE, weight);
+	}
     }
   cout << endl;
   fOutput->Write();
